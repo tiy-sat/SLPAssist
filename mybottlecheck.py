7f87c2cca2b627b80938ae61dba28551d182b46c
Scott McCann <scottmccann@live.com>
2016-05-10 15:22:24 -0500

    routes for index.html and main.css

--- mybottlecheck.py
+++ mybottlecheck.py
@@ -1,13 +1,27 @@
 import os
-from bottle import route, run, template, response, request
+from bottle import route, run, template, response, request, static_file
 import json
 
 
 @route('/')
+@route('/login')
+def serve_index():
+    return static_file('index.html', root='.')
+
+@route('/styles/main.css')
+def serve_css():
+    return static_file('main.css', root='./styles')
+
 @route('/hello')
 @route('/hello/<name>')
 def index(name="Pardner"):
     return template('<div>Howdy, {{person}}!</div>', person=name)
 
 
-run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
+# @route('/dashboard.html')
+# def serve_home():
+#     return static_file('dashboard.html', root='.')
+
+
+run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
