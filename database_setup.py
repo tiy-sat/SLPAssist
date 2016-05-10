dca4eaac509bccfc442840d83ea2f9a85446a3df
David C. Villarreal <redhulk@rocketmail.com>
2016-05-10 15:12:45 -0500

    database setup throught Heroku SLPASSIST

--- database_setup.py
+++ database_setup.py
@@ -1,12 +1,23 @@
 import psycopg2
 import sys
+import os
+import getpass
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from urllib.parse import urlparse
 
+def get_connection():
+    localuser = getpass.getuser()
+    db_url = os.environ.get("DATABASE_URL", None)
+    conn = None
+    if db_url:
+        url = urlparse(db_url)
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port)
 
-con = psycopg2.connect("user=davidvillarreal user=davidvillarreal")
-dbname = "slpassist"
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cur = con.cursor()
-cur.execute('CREATE DATABASE ' + dbname)
-cur.close()
-con.close()
+    else:
+        conn = psycopg2.connect("dbname=slpassist user={}".format(localuser))
+    return conn
