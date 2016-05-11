f6f993cb44344cbe4fe1a2a7160708f9eaeffd46
Paul Lazo <paullazo19@gmail.com>
2016-05-11 15:19:05 -0500

    refactored add-student input fields to dashboard

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+var $ = require("jquery");
+
+var $expandAddStudent = $("[data-js='dashboard_expandAddStudent']");
+var $dashboardInput = $("[data-js='dashboard_input']");
+
 $(function(){
   // Code here!
-})
+  console.log("Sup");
+
+  $expandAddStudent.on("click",function(e){
+    $dashboardInput.toggleClass("hide");
+    console.log(e.target);
+  });
+
+});
