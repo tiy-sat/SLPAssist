7eb09afad27bd254ff949bd0201f162259518241
Mariah Tree <mariah.tree@gmail.com>
2016-05-12 13:42:53 -0500

    working on if statement for >5

--- js/show-students.js
+++ js/show-students.js
@@ -6,20 +6,21 @@ $(function(){
   $.getJSON("/students", function (dataArray){
     dataArray.forEach(function(results){
           $("[data-js='studentListWrapper']").prepend(`
-            <article class="dashboard__student--overview">
+            <article class="dashboard__student--overview" data-js="studentOverview">
               <h3 class="dashboard__student--name" data-js="student_name">${results.stuName}</h3>
-              <span class="dashboard__edit--score">&#9997;</span>
+              <div class="dashboard__edit--score">&#9997;</div>
                 <p class="dashboard__student--scoreStatic">
                   <span class="dashboard__student--scoreDynamic" data-js="student_score">${results.score}</span>/10
                 </p>
             </article>
             `)
-    });
-    $("[data-js='student_score']").each(function(){
-      if (this.val() > 5){
-        this.css("color", "$successGreen");
-      }
+
+    var $score = $("[data-js='student_score']").html();
+
+      if($score > 5){
+         $("[data-js='student_score']").toggleClass("dashboard__student--scoreDynamicGreen");
+      };
 
     });
   });
-})
+});
