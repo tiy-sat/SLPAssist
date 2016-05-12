d9fc49fac48b98715cab4cd0a0654de32486b8b7
Paul Lazo <paullazo19@gmail.com>
2016-05-12 15:26:23 -0500

    revised ajax request to stringify data

--- js/bundle.js
+++ js/bundle.js
@@ -45,7 +45,7 @@ this.ajaxPOST = function(){
       $.ajax({
         method: "POST",
         url: "/students",
-        data: newStudentData,
+        data: JSON.stringify(obj),
         dataType: "json",
         success: function(response){
           console.log(response);
